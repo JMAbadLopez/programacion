c836ec800d540177cf352cefe5f75f7855a83474
Jose Abad <jm.abadlopez@edu.gva.es>
2025-10-06 18:18:59 +0200

    Unidad 3. Funciones y procedimientos.

---
+++ code/ud02/EjemplosUnidad02/src/Ejemplos/EjemploForAnidado.java
@@ -0,0 +1,14 @@
+package ejemplos;
+
+public class EjemploForAnidado {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("Fila (i) : " + i + " Columna (j) :" + j  + " |\t");
+            }
+            System.out.println("");
+        }
+    }
+}
