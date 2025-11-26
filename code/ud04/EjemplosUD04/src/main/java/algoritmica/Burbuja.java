5463f416679d272cff0cc7eb7797d3bf02adba4d
José Abad López <jm.abadlopez@edu.gva.es>
2025-11-26 10:42:09 +0100

    Método de la burbuja

---
+++ code/ud04/EjemplosUD04/src/main/java/algoritmica/Burbuja.java
@@ -0,0 +1,13 @@
+package algoritmica;
+
+public class Burbuja {
+
+    public static void main(String[] args) {
+        int[] numeros = {5,1,4,2,8};
+
+    }
+
+    public static void burbuja(int[] numeros) {
+        
+    }
+}
